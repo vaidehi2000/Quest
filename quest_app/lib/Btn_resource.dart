22067cc302e89e1183db3cd113d3cd5ba110d0ce
Vaidehi Vaishnav <vdvaishnav1222@gmail.com>
2019-12-04 00:43:08 +0530

    Update Btn_resource.dart

--- quest_app/lib/Btn_resource.dart
+++ quest_app/lib/Btn_resource.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class RoundedButton extends StatelessWidget {
+class RoundedButton extends StatelessWidget {         //everytime this class is called the following code runs
   final Color btncolor;
   final Function onPressed;
   final btnText;
@@ -11,19 +11,19 @@ class RoundedButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
+      padding: EdgeInsets.symmetric(vertical: 16.0),      // making edges symmetric
       child: Material(
         elevation: 5.0,
         color: btncolor,
         borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
+        child: MaterialButton(                            //changing features of a button
           onPressed: onPressed,
           minWidth: 200.0,
           height: 42.0,
           child: Text(
             btnText,
             style: TextStyle(
-              color: Colors.white,
+              color: Colors.white,                         //color set to white
             ),
           ),
         ),
